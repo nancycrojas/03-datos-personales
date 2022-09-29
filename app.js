729c79a6f24c2dd1f24e41502da6cbd2875d4d50
nancycrojas <nanclarisaok@gmail.com>
2022-09-29 15:44:45 -0300

    Agregando template strings

--- app.js
+++ app.js
@@ -3,9 +3,12 @@
 //Crear un programa que pida al usuario ingresar, por separado, nombre, apellido, edad, nacionalidad, documento, y muestre luego un mensaje que diga: "Nuevo usuario agregado al sistema:" y liste todos los datos juntos.
 
 
-const nombre = prompt ("Por favor ingresa tu nombre");
-const apellido = prompt ("Por favor ingresa tu apellido");
-const edad = prompt ("Por favor ingresa tu edad");
-const nacionalidad = prompt ("Por favor ingresa tu nacionalidad");
-const documento = prompt ("Por favor ingresa tu numero de documento");
-alert("Nuevo usuario agregado al sistema:" + " " + nombre + ", " + apellido + ", " + edad + ", " + nacionalidad + ", " + "número de DNI: " + documento);
+const nombre = prompt("Por favor ingresa tu nombre");
+const apellido = prompt("Por favor ingresa tu apellido");
+let edad = prompt("Por favor ingresa tu edad");
+const nacionalidad = prompt("Por favor ingresa tu nacionalidad");
+const documento = prompt("Por favor ingresa tu numero de documento");
+//alert("Nuevo usuario agregado al sistema:" + " " + nombre + " " + apellido + ", " + edad + " años, " + nacionalidad + ", " + "DNI: " + documento + ".");
+
+//Template strings
+alert(`Nuevo usuario agregado al sistema: ${nombre} ${apellido}, ${edad} años, ${nacionalidad}, DNI ${documento}.`)
